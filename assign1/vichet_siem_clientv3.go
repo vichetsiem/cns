490944a0e3ec111a78606e5558dc5aecb721ebd0
vichetsiem <37608825+vichetsiem@users.noreply.github.com>
2018-04-03 12:47:57 -0700

    Add files via upload
    assignment 1

---
+++ assign1/vichet_siem_clientv3.go
@@ -0,0 +1,233 @@
+/*
+client.go
+
+Vichet Siem
+Spring 2018
+CYBR 550
+26 MAR 2018
+
+code derived from fenriquez1
+implements the following in
+STEPS
+1) socket establishment, 
+2) case/switch for packet type/condition, 
+3) buffer array check for one password, 
+4) send file (handle little/bigEndian)
+5) terminate connection/hash file
+*/
+
+package main
+
+import (
+	// Packages listed are in alphabetical order
+	// -----------------------------------------
+	
+	// Package sha1 implements the SHA-1 hash algorithm
+	"crypto/sha1" 
+	
+	// Package binary implements simple translation between numbers and byte sequences 
+	// and encoding and decoding of varints.
+	// Will find Little/BigEndian to handle byteOrder
+	"encoding/binary" 
+	
+	// Package fmt implements formatted I/O with functions analogous to C's printf and scanf.
+	"fmt"
+	
+	// Package ioutil implements some I/O utility functions
+	// Will find ReadFile reads the file named by filename and returns the contents
+	"io/ioutil"
+	
+	// Package net provides a portable interface for network I/O, including TCP/IP, UDP, 
+	// domain name resolution, and Unix domain sockets
+	"net"
+	
+	// Package os provides a platform-independent interface to operating system functionality
+	"os"
+	
+	// Package strconv implements conversions to and from string representations of basic data types
+	"strconv"
+)
+
+// Let's define the Functional Specification and Packet Formats
+const (
+	
+	// Functional Specifications
+	// -------------------------
+	// Packet Types are 2-bytes (uint16 / 16-bit integers w/ Range: 0 through 65535.)
+	JOIN_REQ    uint16 = 1
+	PASS_REQ    uint16 = 2
+	PASS_RESP   uint16 = 3
+	PASS_ACCEPT uint16 = 4
+	DATA        uint16 = 5
+	TERMINATE   uint16 = 6
+	REJECT      uint16 = 7
+	
+	// Packet format where int is >= 32-bit (4-bytes)
+	// all packets have 2-byte (packet type) + 4-byte (payload length)
+	// PASSWORD length <= 50-bytes
+	// TERMINATE length = digest (sha1 = 64-bytes)
+	HeaderLength	int    = 2 // couldn't use unit16
+	PayloadLength	int    = 4 // if this was uint32, can't perform add operand
+	PacketidLength	int    = 4 // up to 1000 bytes to handle data
+)
+
+var (
+	server  		[]string
+	userPasswords	[]string
+	outputFile    	string
+	// packet is 6-bytes
+	JOIN_REQ_Array = []byte{0x01, 0x00, 0x00, 0x00, 0x00, 0x00} 
+)
+
+func usage() {
+	// Presents User input format for argument function
+	// text within "< >" are considered arguments
+	// agr[0] = client
+	// agr[1] = server name
+	// agr[2] = server port
+	// agr[3] = clientpwd1
+	// agr[4] = clientpwd2
+	// agr[5] = clientpwd3
+	// agr[6] = output file (creates file with name "output file" from 
+	//	file received from server program
+	
+	fmt.Printf("Usage: ./client <server name> <server port> <clientpwd1> " +
+	"<clientpwd2> <clientpwd3> <output file>\n")
+}
+
+func checkError(err error) {
+	// If there is an error, abort!
+	if err != nil {
+		panic("ABORT")
+	}
+}
+
+
+func checkDigest(packet []byte) {
+	// Check the hash of the file, print "OK" or "ABORT"
+	totalPayloadLength := binary.LittleEndian.Uint32(packet[0:])
+	serverDigest := packet[4:] 
+	// This is 0 because digest starts offset at Byte 7
+	if int(totalPayloadLength) != len(serverDigest) {
+		fmt.Println("ABORT")
+		return
+	}
+
+	data, err := ioutil.ReadFile(outputFile)
+	checkError(err)
+
+	// Perform hash on data
+	digest := sha1.Sum(data)
+
+	if len(serverDigest) != len(digest) {
+		fmt.Println("ABORT")
+		return
+	}
+
+	for i := 0; i < len(digest); i++ {
+		if serverDigest[i] != digest[i] {
+			fmt.Println("ABORT")
+			return
+		}
+	}
+	fmt.Println("OK")
+}
+
+func handleConnection(conn net.Conn) {
+	// Client sends a JOIN_REQ
+	conn.Write(JOIN_REQ_Array)
+
+	// Allocate buffer as byte array, where 
+	// (Header=2)+(Payload=4)+(Packetid=4)+(Data <= 1000) ==1010
+	bufferObject := make([]byte, 1010)
+	
+	// used to cycle through the three user provided passwords, starting at 0
+	password := 0
+
+	// Create file 
+	fileObject, err := os.Create(outputFile)
+	defer fileObject.Close() // Close Create function
+	checkError(err)
+
+	// Pull in user input
+	for {
+		inputLength, err := conn.Read(bufferObject)
+		checkError(err)
+		// handle network byte order
+		headerBytes := binary.LittleEndian.Uint16(bufferObject[0:])
+
+		switch headerBytes {
+		case PASS_REQ:
+			// Handle server password request
+			passRespObject := HeaderLength + PayloadLength + len(userPasswords[password])
+			totalPayloadLength := uint32(len(userPasswords[password]))
+			userInput := make([]byte, passRespObject)
+			binary.LittleEndian.PutUint16(userInput[0:], PASS_RESP)
+			// 0 - 5 Bytes
+			binary.LittleEndian.PutUint32(userInput[2:], totalPayloadLength) 
+			// 
+			copy(userInput[6:], []byte(userPasswords[password]))
+			_, err := conn.Write(userInput)
+			checkError(err)
+			password++
+		case PASS_ACCEPT:
+			// Handle server accepting password
+			// Go to DATA, fallthrough does not work
+		case DATA:
+			// Handle data server sends
+			// Packetid := binary.LittleEndian.Uint32(bufferObject[6:10])
+			data := bufferObject[10:inputLength] 
+			// Data starts at Byte 11
+			_, err := fileObject.Write(data)
+			checkError(err)
+			fileObject.Sync() // Sync flushes writes to stable storage
+		case REJECT:
+			fmt.Println("ABORT")
+			return
+		case TERMINATE:
+			checkDigest(bufferObject[2:inputLength]) // hash starts at Byte 7
+			return
+		default:
+			fmt.Println("ABORT")
+			return
+		}
+	}
+}
+
+func main() {
+	// Define the arguments to be parsed by program w/o using flags
+	// Flags could be "./client -what -do -you -want", 
+	// instead, we'll skip arg[0] which would return "client"
+	//
+	// See example: https://gobyexample.com/command-line-arguments
+	//
+	args := os.Args
+	if len(args) != 7 {
+		usage() // parses argument from User input
+		return
+	}
+
+	// Parse command line argument strings (args)
+	server = args[1:3] 			// <server name> <server port>
+	userPasswords = args[3:6] 	// <clientpwd1><clientpwd2><clientpwd3>
+	outputFile = args[6] 		// <output file>
+
+	// Convert string (server[1]) to integer
+	_, err := strconv.Atoi(server[1])
+	if err != nil {
+		// if there is no error, print to screen expected command arguments
+		usage()
+		return
+	}
+
+	// Connect to server
+	serverAddrPort := server[0] + ":" + server[1]
+	
+	// Dial function is "tcp/udp" "golang.org:80"
+	// establish connection
+	conn, err := net.Dial("udp4", serverAddrPort)
+	defer conn.Close() // defer ensures connection is closed
+	checkError(err)
+
+	handleConnection(conn)
+}
---
+++ assign1/vichet_siem_serverv3.go
@@ -0,0 +1,230 @@
+/*
+server.go
+
+Vichet Siem
+Spring 2018
+CYBR 550
+26 MAR 2018
+
+code derived from fenriquez1
+implements the following in
+STEPS
+1) socket establishment, 
+2) case/switch for packet type/condition, 
+3) buffer array check for one password, 
+4) send file (handle little/bigEndian)
+5) terminate connection/hash file
+*/
+
+
+package main
+
+import (
+	// Packages listed are in alphabetical order
+	// -----------------------------------------
+	
+	// Package sha1 implements the SHA-1 hash algorithm
+	"crypto/sha1" 
+	
+	// Package binary implements simple translation between numbers and byte sequences 
+	// and encoding and decoding of varints.
+	// Will find Little/BigEndian to handle byteOrder
+	"encoding/binary" 
+	
+	// Package fmt implements formatted I/O with functions analogous to C's printf and scanf.
+	"fmt"
+	
+	// Package ioutil implements some I/O utility functions
+	// Will find ReadFile reads the file named by filename and returns the contents
+	"io/ioutil"
+	
+	// Package net provides a portable interface for network I/O, including TCP/IP, UDP, 
+	// domain name resolution, and Unix domain sockets
+	"net"
+	
+	// Package os provides a platform-independent interface to operating system functionality
+	"os"
+	
+	// Package strconv implements conversions to and from string representations of basic data types
+	"strconv"
+
+	// Package strings implements simple functions to manipulate UTF-8 encoded strings.
+	"strings"
+)
+
+// Let's define the Functional Specification and Packet Formats
+const (
+	
+	// Functional Specifications
+	// -------------------------
+	// Packet Types are 2-bytes (uint16 / 16-bit integers w/ Range: 0 through 65535.)
+	JOIN_REQ    uint16 = 1
+	PASS_REQ    uint16 = 2
+	PASS_RESP   uint16 = 3
+	PASS_ACCEPT uint16 = 4
+	DATA        uint16 = 5
+	TERMINATE   uint16 = 6
+	REJECT      uint16 = 7
+	
+	// Packet format where int is >= 32-bit (4-bytes)
+	// all packets have 2-byte (packet type) + 4-byte (payload length)
+	// PASSWORD length <= 50-bytes
+	// TERMINATE length = digest (sha1 = 64-bytes)
+	HeaderLength	int    = 2
+	PayloadLength	int    = 4
+	PacketidLength	int    = 4 // up to 1000 bytes to handle data
+)
+
+var (
+	serverPassword	string
+	inputFile	string
+	PASS_REQ_Array     	= []byte{0x02, 0x00, 0x00, 0x00, 0x00, 0x00}
+	PASS_ACCEPT_Array  	= []byte{0x04, 0x00, 0x00, 0x00, 0x00, 0x00}
+	REJECT_Array      	= []byte{0x07, 0x00, 0x00, 0x00, 0x00, 0x00}
+)
+
+func usage() {
+	// Presents User input format for argument function
+	// text within "< >" are considered arguments
+	// agr[0] = server
+	// agr[1] = server port
+	// agr[2] = password
+	// agr[3] = input file (open file to send to client)
+	fmt.Printf("Usage: ./server <port> <password> <input file>\n")
+}
+
+func checkError(err error) {
+	// If there is an error, abort!
+	if err != nil {
+		panic("ABORT")
+	}
+}
+
+func sendFile(conn net.PacketConn, serverAddr net.Addr) {
+	// Read file from present directory and send
+	// NOTE: file must be in working directory or this will fail
+	fileObject, err := os.Open(inputFile)
+	defer fileObject.Close()
+	checkError(err)
+
+	inputFileObject, err := fileObject.Stat() // get FileInfo on source file
+	checkError(err)
+
+	dataObject := make([]byte, 1000)
+	size := inputFileObject.Size()
+	Packetid := uint32(0)
+	for i := int64(0); i < size; {
+		dobj, err := fileObject.Read(dataObject)
+		checkError(err)
+		packetLength := HeaderLength + PayloadLength + PacketidLength + dobj
+		packet := make([]byte, packetLength)
+		binary.LittleEndian.PutUint16(packet[0:], DATA)
+		binary.LittleEndian.PutUint32(packet[2:], uint32(dobj))
+		binary.LittleEndian.PutUint32(packet[6:], Packetid)
+		copy(packet[10:], dataObject[0:dobj])
+		conn.WriteTo(packet, serverAddr)
+		i += int64(dobj)
+		Packetid++
+	}
+}
+
+func terminate(conn net.PacketConn, serverAddr net.Addr) {
+	// Get digest and send TERMINATE packet
+	data, err := ioutil.ReadFile(inputFile)
+	checkError(err)
+	
+    // Perform hash on data
+	digest := sha1.Sum(data)
+
+	packetLength := HeaderLength + PayloadLength + sha1.Size
+	packet := make([]byte, packetLength)
+	binary.LittleEndian.PutUint16(packet[0:], TERMINATE)
+	binary.LittleEndian.PutUint32(packet[2:], uint32(len(digest)))
+	copy(packet[6:], digest[0:])
+	conn.WriteTo(packet, serverAddr)
+	fmt.Println("OK")
+}
+
+func handlePacketConnection(conn net.PacketConn) {
+	request := 0
+	// Allocate buffer as byte array, where 
+	// (HeaderLength==2) +(PayloadLength==4) +(PacketidLength==4)+ (Data <= 1000) ==1010
+	bufferObject := make([]byte, 1010)
+
+	for {
+		count, serverAddress, err := conn.ReadFrom(bufferObject)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		frmClient := bufferObject[0:count]
+		headerByte := binary.LittleEndian.Uint16(bufferObject[0:])
+
+		switch headerByte {
+		case JOIN_REQ:
+			conn.WriteTo(PASS_REQ_Array, serverAddress)
+			request++
+		case PASS_RESP:
+			clientPassword := string(frmClient[6:])
+			if strings.Compare(clientPassword, serverPassword) == 0 {
+				conn.WriteTo(PASS_REQ_Array, serverAddress)
+				// Password matches, send file to client
+				sendFile(conn, serverAddress)
+				// Terminate connection after completion
+				terminate(conn, serverAddress)
+				return
+			}
+			if request < 3 {
+				// allow no more than three password tries, else bomb out
+				conn.WriteTo(PASS_REQ_Array, serverAddress)
+				request++
+			} else {
+				conn.WriteTo(REJECT_Array, serverAddress)
+				fmt.Println("ABORT")
+				return
+			}
+		default:
+			fmt.Println("ABORT")
+			return
+		}
+		// For testing buffer comparison
+		//fmt.Printf("Server Address = %s\n", serverAddress)
+		//fmt.Printf("Buffer = %#4x\n", frmClient)
+	}
+}
+
+func main() {
+	// Define the arguments to be parsed by program w/o using flags
+	// Flags could be "./server -what -do -you -want", 
+	// instead, we'll skip arg[0] which would return "server"
+	//
+	// See example: https://gobyexample.com/command-line-arguments
+	//
+	args := os.Args
+	if len(args) != 4 {
+		usage() // parses argument from User input
+		return
+	}
+
+	// Parse command line argument strings (args)
+	localPort := args[1] 		// <server port>
+	serverPassword = args[2] 	// <password>
+	inputFile = args[3] 	// <input file>
+
+	// Convert string (serverPort) to integer
+	_, err := strconv.Atoi(localPort)
+	if err != nil {
+		// if there is no error, print to screen expected command arguments
+		usage()
+		return
+	}
+
+	// Setup addressing for listening call
+	serverAddr := ":" + localPort
+	// Listen for client to establish connection
+	conn, err := net.ListenPacket("udp4", serverAddr)
+	defer conn.Close() // defer ensures connection is closed
+	checkError(err)
+
+	handlePacketConnection(conn)
+}
